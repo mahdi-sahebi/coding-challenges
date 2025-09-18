7dba1c99aec7cb748c55e385ff60e098fc9b61ae
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-09-18 20:20:28 +0100

    [UPDATE]: Result types are defined

--- counting_uniques_in_file/include/common/error.h
+++ counting_uniques_in_file/include/common/error.h
@@ -2,12 +2,15 @@
 #define ERROR_H_
 
 #include <stdint.h>
+#include <stdbool.h>
 
 
 typedef enum
 {
   MC_SUCCESS = 0,
-  MC_ERR_RUNTIME
+  MC_ERR_RUNTIME,
+  MC_ERR_BAD_ALLOC,
+  MC_ERR_INVALID_ARGUMENT,
 }error_t;
 
 
@@ -16,6 +19,18 @@ typedef struct
   uint32_t value;
   error_t  error;
 }result_u32;
+
+typedef struct
+{
+  bool value;
+  error_t  error;
+}result_bit;
+
+typedef struct
+{
+  void*    value;
+  error_t  error;
+}result_ptr;
 
 
 #endif /* ERROR_H_ */
