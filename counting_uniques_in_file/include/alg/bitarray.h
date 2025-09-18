109c381262eaecaffa9717fcd48e8b30cdabe018
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-09-18 20:12:33 +0100

    [UPDATE]: Bit array module is implemented

--- counting_uniques_in_file/include/alg/bitarray.h
+++ counting_uniques_in_file/include/alg/bitarray.h
@@ -1,6 +1,23 @@
 #ifndef ALGORITHM_BITARRAY_H_
 #define ALGORITHM_BITARRAY_H_
 
+#include <stdint.h>
+#include <stdbool.h>
+
+
+typedef struct
+{
+  uint32_t capacity;
+  uint8_t  data[0];
+}bitarr_t;
+
+
+
+bitarr_t* bitarr_t_create(uint32_t capacity);
+void      bitarr_t_destroy(bitarr_t* this);
+void      bitarr_t_clear(bitarr_t* this);
+void      bitarr_t_set(bitarr_t* this, uint32_t index, bool bit);
+bool      bitarr_t_get(bitarr_t* this, uint32_t index);
 
 
 #endif /* ALGORITHM_BITARRAY_H_ */
